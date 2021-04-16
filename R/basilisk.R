c05760e2e29408f632076ac46193338ef29136da
Max <38260677+maximilian-heeg@users.noreply.github.com>
2021-04-16 10:45:28 -0700

    Update basilisk.R
    update magic-impute version in conda environment to 3.0.0

--- R/basilisk.R
+++ R/basilisk.R
@@ -7,4 +7,4 @@
 #' @import basilisk
 magieR.env <- BasiliskEnvironment("env", "magieR",
                                 packages=.magieR_dependencies, channels = c("bioconda", "conda-forge"),
-                                pip=c("scipy==1.5.4", "magic-impute==2.0.4"))
+                                pip=c("scipy==1.5.4", "magic-impute==3.0.0"))
