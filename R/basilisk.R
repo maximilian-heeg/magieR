53f984c57a8d9dac37bde8e62c85a0df51f8e43f
Max <38260677+maximilian-heeg@users.noreply.github.com>
2024-05-17 17:17:26 -0700

    Update basilisk.R

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,10 +1,8 @@
 .magieR_dependencies <- c(
- "numpy==1.19.2",
- "pandas==1.2.0"
 )
 
 
 #' @import basilisk
 magieR.env <- BasiliskEnvironment("env", "magieR",
                                 packages=.magieR_dependencies, channels = c("bioconda", "conda-forge"),
-                                pip=c("scipy==1.5.4", "magic-impute==3.0.0"))
+                                pip=c("magic-impute==3.0.0"))
